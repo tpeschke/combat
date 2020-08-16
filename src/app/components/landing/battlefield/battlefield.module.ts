a5f9f5d9ee351755ad67a743121fc30453deb9b9
Trent <mr.peschke@gmail.com>
2020-08-16 13:39:46 -0600

    fixed the styling on inputs

--- src/app/components/landing/battlefield/battlefield.module.ts
+++ src/app/components/landing/battlefield/battlefield.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { BattlefieldComponent } from './battlefield/battlefield.component';
-import { MatCardModule, MatButtonModule, MatTooltipModule, MatCheckboxModule, MatDialogModule, MatExpansionModule } from '@angular/material';
+import { MatCardModule, MatButtonModule, MatTooltipModule, MatCheckboxModule, MatDialogModule, MatExpansionModule, MatFormFieldModule, MatInputModule } from '@angular/material';
 import { CounterComponent } from './counter/counter.component';
 import { RouterModule } from '@angular/router';
 import { FightersectionComponent } from './fighter-section/fightersection.component';
@@ -17,6 +17,8 @@ import { WeaponSelectComponent } from './utils/weapon-select/weapon-select.compo
     MatCheckboxModule,
     MatDialogModule,
     MatButtonModule,
+    MatFormFieldModule,
+    MatInputModule,
     MatExpansionModule
   ],
   exports: [
