8b92ccf69c577a036f28c3ba183921b6050cefc4
Trent <mr.peschke@gmail.com>
2020-08-17 17:43:56 -0600

    damage can now be changed and it updates, which is nice

--- src/app/components/landing/battlefield/fighter-section/fightersection.component.ts
+++ src/app/components/landing/battlefield/fighter-section/fightersection.component.ts
@@ -3,6 +3,7 @@ import { CounterService } from 'src/app/utils/counter.service';
 import { FieldService } from 'src/app/utils/field.service';
 import { MatDialog } from '@angular/material';
 import { WeaponSelectComponent } from '../utils/weapon-select/weapon-select.component';
+import { SwitchView } from '@angular/common/src/directives/ng_switch';
 
 @Component({
   selector: 'app-fighter-section',
@@ -42,6 +43,29 @@ export class FightersectionComponent implements OnInit {
           this.fieldService.sendBattleData({ hash: this.counterService.hash, type: 'fighterChange', value: '1', id, fighterProperty: 'hidden' })
         }
         this.counterService.sort()
+        i = fighters.length
+      }
+    }
+  }
+
+  changeDamage(event, fighterId) {
+    let { fighters } = this.counterService
+    for (let i = 0; i < fighters.length; i++) {
+      if (fighters[i].id === fighterId) {
+        fighters[i].health = +event.target.value
+        let wound = +event.target.value === 0 ? '00' : +event.target.value * 100 / fighters[i].max_health;
+        if (wound > 0 && wound < 25) {
+          wound = 10
+        } else if (wound >= 25 && wound < 50) {
+          wound = 25
+        } else if (wound >= 50 && wound < 75) {
+          wound = 50
+        } else if (wound >= 75 && wound < 100) {
+          wound = 75
+        } else if (wound >= 100) {
+          wound = ''
+        }
+        this.fieldService.sendBattleData({ hash: this.counterService.hash, type: 'fighterChange', value: wound, id: fighterId, fighterProperty: 'wound' })
         i = fighters.length
       }
     }
