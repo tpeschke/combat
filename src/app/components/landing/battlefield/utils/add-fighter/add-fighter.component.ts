7016c25dfda0a24b439ff2f6c01631254ddc8a98
Trent <mr.peschke@gmail.com>
2020-08-20 09:09:13 -0600

    multi add and unique colors now work

--- src/app/components/landing/battlefield/utils/add-fighter/add-fighter.component.ts
+++ src/app/components/landing/battlefield/utils/add-fighter/add-fighter.component.ts
@@ -1,6 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChildren, QueryList } from '@angular/core';
 import { WeaponSelectComponent } from '../weapon-select/weapon-select.component';
-import { MatDialog } from '@angular/material';
+import { MatDialog, MatExpansionPanel } from '@angular/material';
 import { CounterService } from 'src/app/utils/counter.service';
 import { GeneralService } from 'src/app/utils/general.service';
 
@@ -10,6 +10,7 @@ import { GeneralService } from 'src/app/utils/general.service';
   styleUrls: ['./add-fighter.component.css']
 })
 export class AddFighterComponent implements OnInit {
+  @ViewChildren(MatExpansionPanel) viewPanels: QueryList<MatExpansionPanel>;
 
   constructor(
     private dialog: MatDialog,
@@ -43,7 +44,9 @@ export class AddFighterComponent implements OnInit {
   public uniqueColors = false
   public numberEach = false
 
-  ngOnInit() { }
+  ngOnInit() {
+    this.fighter.selected = this.fighter.weapons[0]
+  }
 
   captureChange(value, type) {
     this.fighter[type] = value
@@ -85,37 +88,66 @@ export class AddFighterComponent implements OnInit {
   }
 
   addFighter() {
-    if (this.multiAdd && this.multiAdd > 1) {
-      let colors = ['#C91010', '#1076C9', '#2889e9', '#2FC910', '#C97310', '#9510C9', '#EB75E1', '#E5EB75']
+    if (this.checkIfFighterIsValid()) {
+      if (this.multiAdd && this.multiAdd > 1) {
+        let colors = ['#C91010', '#1076C9', '#2889e9', '#2FC910', '#C97310', '#9510C9', '#EB75E1', '#E5EB75']
+        let newFighters = []
+        for (let i = 0; i < this.multiAdd; i++) {
+          let fighterCopy = {...this.fighter}
+          fighterCopy.id = this.generalService.makeid()
+          if (this.numberEach) { fighterCopy.namefighter = fighterCopy.namefighter + ` ${i + 1}` }
+          if (this.uniqueColors) { 
+            i > 7 ? fighterCopy.colorcode = this.generalService.genHexString() : fighterCopy.colorcode = colors[i]
+          }
+          newFighters.push({ ...fighterCopy })
+        }
+        this.counterService.addFighter(newFighters);
+        // add fighters to player view
+        this.counterService.sort()
+      } else {
+        this.fighter.id = this.generalService.makeid()
+        this.counterService.addFighter([this.fighter]);
+        // add fighter to player view
+        this.counterService.sort()
+      }
+      this.viewPanels.forEach(p => p.close());
+      this.multiAdd = null
+      this.uniqueColors = false
+      this.numberEach = false
+      let newId = this.generalService.makeid()
+      this.fighter = {
+        id: null,
+        hidden: '0',
+        colorcode: '#000000',
+        namefighter: '',
+        health: 0,
+        max_health: 0,
+        stress: 0,
+        dead: '0',
+        acting: '0',
+        stressthreshold: 0,
+        weapons: [{
+          id: newId,
+          weapon: 'Unarmed',
+          speed: 10,
+          encumb: 10,
+          selected: '1',
+          init: 0
+        }],
+        selected: {
+          id: newId,
+          weapon: 'Unarmed',
+          speed: 10,
+          encumb: 10,
+          selected: '1',
+          init: 0
+        },
+        actioncount: [1]
+      }
+    }
+  }
 
-    } else {
-      this.fighter.id = this.generalService.makeid()
-      this.counterService.addFighter([this.fighter]);
-      // add fighter to player view
-      console.log(this.counterService.fighters)
-      this.counterService.sort()
-    }
-    this.fighter = {
-      id: null,
-      hidden: '0',
-      colorcode: '#000000',
-      namefighter: '',
-      health: 0,
-      max_health: 0,
-      stress: 0,
-      dead: '0',
-      acting: '0',
-      stressthreshold: 0,
-      weapons: [{
-        id: this.generalService.makeid(),
-        weapon: 'Unarmed',
-        speed: 10,
-        encumb: 10,
-        selected: '1',
-        init: 0
-      }],
-      selected: null,
-      actioncount: [1]
-    }
+  checkIfFighterIsValid() {
+    return true
   }
 }
