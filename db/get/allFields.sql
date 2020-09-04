4fa9d1aa959baac9b66cffd53370ec71c32e7b7f
Trent <mr.peschke@gmail.com>
2020-09-04 14:52:17 -0600

    fixing the count issue on the fields pages

--- db/get/allFields.sql
+++ db/get/allFields.sql
@@ -1,4 +1,4 @@
-select namecombat as name, sum(countnum) as count, count(namefighter) as fightercount, sum(cast(dead as INT)) as deadcount, sum(distinct combat.id) id, urlhash as hash from combat
+select namecombat as name, max(countnum) as count, count(namefighter) as fightercount, sum(cast(dead as INT)) as deadcount, sum(distinct combat.id) id, urlhash as hash from combat
 full join combatants on combatants.idcombat = combat.id
 where iduser = $1
 group by namecombat, urlhash
